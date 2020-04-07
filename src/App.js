5b9a11b00e6fb9fb07167d3c98018e90c4aca28c
mj <hi.minjungkim@gmail.com>
2020-04-07 16:02:56 +0900

    Delete content

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode: 'create',
+      mode: 'welcome',
       selected_content_id: 2,
       subject: {title:'WEB', sub:'World wide web!'},
       welcome: {title:'welcome', desc:'hello react'},
@@ -123,9 +123,28 @@ import './App.css';
           data={this.state.contents}
         ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode: _mode
-          })
+          if(_mode === 'delete') {
+            if (window.confirm('삭제하시겠습니까?')) {
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length) {
+                if (_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i += 1;
+              }
+              this.setState({
+                mode: 'welcome',
+                contents:_contents
+              });
+              alert('삭제 성공');
+            }
+          } else {
+            this.setState({
+              mode: _mode
+            });
+          }
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
